9f1183dddb6a41082c76e82d9834873e26d5672e
ememra <emj1025@yahoo.co.jp>
2019-08-28 17:03:26 +0900

    SPナビ修正

--- nuxt-contentful-blog/components/Header.vue
+++ nuxt-contentful-blog/components/Header.vue
@@ -2,7 +2,9 @@
 	<header id="l-header">
 		<div class="l-header-in">
 			<h1 class="g-logo">
-				Emura Design Portfolio
+				<nuxt-link to="/" exact>
+					Emura Design Portfolio
+				</nuxt-link>
 			</h1>
 			<nav class="g-main-navi">
 				<button class="g-main-navi-btn" @click="naviOpen" :class="{'is-active': active}" aria-controls="g-menu" aria-label="メニュー">
@@ -14,29 +16,29 @@
 				<div class="g-main-navi-wrap js-main-navi" id="g-menu" :class="{'is-active': navi}">
 					<ul class="g-main-navi-list">
 						<li class="g-main-navi-list__item">
-							<nuxt-link to="/" active-class="is-active" class="g-main-navi-list__link" exact>
+							<nuxt-link to="/" @click.native="naviOpen" active-class="is-active" class="g-main-navi-list__link" exact>
 								Top
 							</nuxt-link>
 						</li>
 						<li class="g-main-navi-list__item">
-							<nuxt-link to="/profile/" active-class="is-active" class="g-main-navi-list__link" exact>
+							<nuxt-link to="/profile/" @click.native="naviOpen" active-class="is-active" class="g-main-navi-list__link" exact>
 								Profile
 							</nuxt-link>
 						</li>
 						<li class="g-main-navi-list__item">
-							<nuxt-link to="/works/" active-class="is-active" class="g-main-navi-list__link" exact>
+							<nuxt-link to="/works/" @click.native="naviOpen" active-class="is-active" class="g-main-navi-list__link" exact>
 								Works
 							</nuxt-link>
 						</li>
 						<li class="g-main-navi-list__item">
-							<nuxt-link to="/thinking/" active-class="is-active" class="g-main-navi-list__link" exact>
+							<nuxt-link to="/thinking/" @click.native="naviOpen" active-class="is-active" class="g-main-navi-list__link" exact>
 								Thinking
 							</nuxt-link>
 						</li>
 					</ul>
-					<button class="g-main-navi-btn-close js-main-navi-btn-close">
+<!-- 					<button class="g-main-navi-btn-close js-main-navi-btn-close">
 						<img src="{gl.top}/images/icon-close.svg" alt="" width="15" height="15" /> close
-					</button>
+					</button> -->
 				</div><!-- /.main-navi-wrap-->
 			</nav>
 		</div>
