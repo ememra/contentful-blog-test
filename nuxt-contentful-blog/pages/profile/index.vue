3616e72046311179285c7a1348ac88780df4e93e
ememra <emj1025@yahoo.co.jp>
2019-12-12 17:02:54 +0900

    プロフィールの回転CSSが抜けていたので対応

--- nuxt-contentful-blog/pages/profile/index.vue
+++ nuxt-contentful-blog/pages/profile/index.vue
@@ -405,5 +405,52 @@ export default {
 
 
 <style lang="scss">
+.card-item {
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 90px;
+  position: relative;
+  z-index: 2;
+  width: 100%;
+  cursor: pointer;
+
+  &.is-active {
+    & .card-item__side {
+      &.-front {
+        transform: perspective(1000px) rotateY(180deg) rotateX(0deg) rotateZ(0deg);
+      }
+      &.-back {
+        transform: perspective(1000px) rotateY(0) rotateX(0deg) rotateZ(0deg);
+      }
+    }
+  }
+}
+
+.card-item__side {
+	box-shadow: $shadow;
+	transform: perspective(2000px) rotateY(0deg) rotateX(0deg) rotate(0deg);
+	transform-style: preserve-3d;
+	transition: all 0.5s cubic-bezier(0.71, 0.03, 0.56, 0.85);
+	backface-visibility: hidden;
+	height: 100%;
+
+	&.-back {
+	  position: absolute;
+	  top: 0;
+	  left: 0;
+	  width: 100%;
+	  transform: perspective(2000px) rotateY(-180deg) rotateX(0deg) rotate(0deg);
+	  z-index: 2;
+	  padding: 0;
+	  height: 100%;
+
+	  .card-item__cover {
+	    transform: rotateY(-180deg)
+	  }
+	}
+}
+
+
 
 </style>
